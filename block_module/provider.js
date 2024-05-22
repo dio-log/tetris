8d1b7dc98ed04cbfe49f40765eccc7b9f3b03560
dio-log <ae7006@gmail.com>
2024-05-22 18:25:08 +0900

    provider 파일 추가 및 샘플코드 구현

---
+++ block_module/provider.js
@@ -0,0 +1,11 @@
+export { OShape };
+class OShape {
+    generate() {
+        return [
+            { x: 0, y: 0 },
+            { x: 10, y: 0 },
+            { x: 0, y: 10 },
+            { x: 10, y: 10 },
+        ];
+    }
+}
