0235d9f748ea6fd0c71fbb6839dec5d79ce68282
dio-log <ae7006@gmail.com>
2024-05-27 19:10:08 +0900

    폴더명 변경으로인한 경로변경

--- block_module/type.ts
+++
@@ -1,5 +0,0 @@
-export{ XY, Predicate };
-
-type XY = {x: number, y: number};
-
-type Predicate<T> = (param: T) => boolean;
--- block_module/util.js
+++
@@ -1,9 +0,0 @@
-export { Converter };
-class Converter {
-    constructor(callback) {
-        this._callback = callback;
-    }
-    convert(source, param) {
-        return this._callback(source, param);
-    }
-}
